e1812f4582082134900b985e7872a6482cb19c1f
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-02 12:40:41 +0200

    Design changed, one color and animated titles

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         currentlyOpen.classList.remove('open');
         currentlyOpen.classList.add('shrink');
         currentlyOpen.style.height = '16.67vh';
+
+        // Show the h1 element in the currently open accordion
+        const currentlyOpenTitle = currentlyOpen.querySelector('h1');
+        currentlyOpenTitle.classList.remove('opacity-0');
       }
 
       // If the clicked accordion was not the one that was open, open it
@@ -17,6 +21,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         accordion.classList.remove('shrink');
         accordion.classList.add('open');
         accordion.style.height = '50vh';
+
+        // Hide the h1 element in the clicked accordion
+        const accordionTitle = accordion.querySelector('h1');
+        accordionTitle.classList.add('opacity-0');
       } else {
         // If the clicked accordion was the one that was open, close it
         accordion.classList.remove('open');
@@ -30,6 +38,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
           otherAccordion.classList.remove('open');
           otherAccordion.classList.add('shrink');
           otherAccordion.style.height = accordion.classList.contains('open') ? '10vh' : '16.67vh';
+
+          // Show the h1 element in the other accordions
+          const otherAccordionTitle = otherAccordion.querySelector('h1');
+          otherAccordionTitle.classList.remove('opacity-0');
         }
       });
     });
