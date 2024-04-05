68e5b77722d4364b1df81aa6c30a2e7aa440bd8e
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-05 14:09:39 +0200

    add to functions resetbodycolor and changebodycolor to change the body

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,30 @@
+const body = document.querySelector('body')
 const accordions = Array.from(document.querySelectorAll('.accordion'));
+
+body.classList.add('transition', 'ease-in-out', 'duration-300');
+
+function resetBodyColor() {
+  body.classList.remove('bg-green-200', 'bg-blue-200', 'bg-red-200', 'bg-yellow-200', 'bg-purple-200', 'bg-indigo-200');
+  body.classList.add('bg-stone-200');
+}
+
+function changeBodyColor(accordion) {
+  body.classList.remove('bg-green-200', 'bg-blue-200', 'bg-red-200', 'bg-yellow-200', 'bg-purple-200', 'bg-indigo-200', 'bg-stone-200');
+
+  if (accordion.classList.contains('bio-main')) {
+    body.classList.add('bg-green-200');
+  } else if (accordion.classList.contains('studies-main')) {
+    body.classList.add('bg-blue-200');
+  } else if (accordion.classList.contains('work-main')) {
+    body.classList.add('bg-red-200');
+  } else if (accordion.classList.contains('projects-main')) {
+    body.classList.add('bg-yellow-200');
+  } else if (accordion.classList.contains('social-main')) {
+    body.classList.add('bg-purple-200');
+  } else if (accordion.classList.contains('contact-main')) {
+    body.classList.add('bg-indigo-200');
+  }
+}
 
 accordions.forEach((accordion) => {
   const title = accordion.querySelector('.accordion-title');
@@ -7,6 +33,9 @@ accordions.forEach((accordion) => {
     if (accordion.classList.contains('open')) {
       // Si el acordeón ya está abierto, quitarle la clase 'open'
       accordion.classList.remove('open');
+
+       // Restablecer el color de fondo del cuerpo a stone-200
+       resetBodyColor();
 
       // Hacer que el título reaparezca
       setTimeout(() => {
@@ -20,10 +49,13 @@ accordions.forEach((accordion) => {
       accordions.forEach((otherAccordion) => {
         otherAccordion.classList.remove('shrink');
       });
+
     } else {
       // Si el acordeón no está abierto o tiene la clase 'shrink', añadirle la clase 'open' y quitarle la clase 'shrink'
       accordion.classList.add('open');
       accordion.classList.remove('shrink');
+      changeBodyColor(accordion);
+
 
       // Hacer que el título se desvanezca y luego se oculte
       title.style.opacity = '0';
