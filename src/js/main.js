2e2c0b32877de5be36eedae9379528ddd60ef181
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-05 14:53:48 +0200

    All transitions and heights seems to be ok

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,12 @@ const accordions = Array.from(document.querySelectorAll('.accordion'));
 
 body.classList.add('transition', 'ease-in-out', 'duration-300');
 
-function resetBodyColor() {
+const resetBodyColor = () => {
   body.classList.remove('bg-green-200', 'bg-blue-200', 'bg-red-200', 'bg-yellow-200', 'bg-purple-200', 'bg-indigo-200');
   body.classList.add('bg-stone-200');
 }
 
-function changeBodyColor(accordion) {
+const changeBodyColor = (accordion) => {
   body.classList.remove('bg-green-200', 'bg-blue-200', 'bg-red-200', 'bg-yellow-200', 'bg-purple-200', 'bg-indigo-200', 'bg-stone-200');
 
   if (accordion.classList.contains('bio-main')) {
@@ -26,59 +26,60 @@ function changeBodyColor(accordion) {
   }
 }
 
+const openAccordion = (accordion, accordions, title) => {
+  accordions.forEach((otherAccordion) => {
+    if (otherAccordion !== accordion && otherAccordion.classList.contains('open')) {
+      const otherTitle = otherAccordion.querySelector('.accordion-title');
+      closeAccordion(otherAccordion, accordions, otherTitle);
+    }
+  });
+
+  accordion.classList.add('open');
+  accordion.classList.remove('shrink');
+  changeBodyColor(accordion);
+
+  title.style.opacity = '0';
+  setTimeout(() => {
+    title.style.display = 'none';
+  }, 300);
+
+  accordions.forEach((otherAccordion) => {
+    if (otherAccordion !== accordion) {
+      otherAccordion.classList.add('shrink');
+      const otherTitle = otherAccordion.querySelector('.accordion-title');
+      otherTitle.style.display = 'flex';
+      setTimeout(() => {
+        otherTitle.style.opacity = '1';
+      }, 300);
+    }
+  });
+}
+
+const closeAccordion = (accordion, accordions, title) => {
+  accordion.classList.remove('open');
+  accordion.classList.remove('shrink');
+  resetBodyColor();
+
+  setTimeout(() => {
+    title.style.display = 'flex';
+    setTimeout(() => {
+      title.style.opacity = '1';
+    }, 300);
+  }, 300);
+
+  accordions.forEach((otherAccordion) => {
+    otherAccordion.classList.remove('shrink');
+  });
+}
+
 accordions.forEach((accordion) => {
   const title = accordion.querySelector('.accordion-title');
 
   accordion.addEventListener('click', () => {
     if (accordion.classList.contains('open')) {
-      // Si el acordeón ya está abierto, quitarle la clase 'open'
-      accordion.classList.remove('open');
-
-       // Restablecer el color de fondo del cuerpo a stone-200
-       resetBodyColor();
-
-      // Hacer que el título reaparezca
-      setTimeout(() => {
-        title.style.display = 'flex';
-        setTimeout(() => {
-          title.style.opacity = '1';
-        }, 100); // Este tiempo puede ser ajustado para controlar la velocidad de la transición de opacidad
-      }, 300); // Este tiempo debe coincidir con la duración de la transición
-
-      // A cada acordeón, quitarle la clase 'shrink'
-      accordions.forEach((otherAccordion) => {
-        otherAccordion.classList.remove('shrink');
-      });
-
+      closeAccordion(accordion, accordions, title);
     } else {
-      // Si el acordeón no está abierto o tiene la clase 'shrink', añadirle la clase 'open' y quitarle la clase 'shrink'
-      accordion.classList.add('open');
-      accordion.classList.remove('shrink');
-      changeBodyColor(accordion);
-
-
-      // Hacer que el título se desvanezca y luego se oculte
-      title.style.opacity = '0';
-      setTimeout(() => {
-        title.style.display = 'none';
-      }, 300); // Este tiempo debe coincidir con la duración de la transición
-
-      // A cada acordeón que no sea el clicado, añadirle la clase 'shrink' y quitarle la clase 'open'
-      accordions.forEach((otherAccordion) => {
-        if (otherAccordion !== accordion) {
-          otherAccordion.classList.add('shrink');
-          otherAccordion.classList.remove('open');
-
-          // Hacer que el título del acordeón anteriormente abierto reaparezca
-          const otherTitle = otherAccordion.querySelector('.accordion-title');
-          setTimeout(() => {
-            otherTitle.style.display = 'flex';
-            setTimeout(() => {
-              otherTitle.style.opacity = '1';
-            }, 100); // Este tiempo puede ser ajustado para controlar la velocidad de la transición de opacidad
-          }, 300); // Este tiempo debe coincidir con la duración de la transición
-        }
-      });
+      openAccordion(accordion, accordions, title);
     }
   });
 });
