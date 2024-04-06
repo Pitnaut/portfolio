44c8f4114637b3cbf78669881c00214e6a602e84
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-06 13:04:39 +0200

    Add shrink and open classes to tailwind.config.mjs and deleted from styles.css

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 const body = document.querySelector('body')
 const accordions = Array.from(document.querySelectorAll('.accordion'));
+const sectionsContent = {
+  'bio-main': document.querySelector('.bio-content'),
+  'studies-main': document.querySelector('.studies-content'),
+  'work-main': document.querySelector('.work-content'),
+  'projects-main': document.querySelector('.projects-content'),
+  'social-main': document.querySelector('.social-content'),
+  'contact-main': document.querySelector('.contact-content'),
+};
 
 body.classList.add('transition', 'ease-in-out', 'duration-300');
 
@@ -26,57 +34,80 @@ const changeBodyColor = (accordion) => {
   }
 }
 
+const toggleContent = (section, show) => {
+  const content = sectionsContent[section];
+  if (content) {
+    if (show) {
+      setTimeout(() => {
+        content.classList.remove('hidden');
+        content.style.opacity = '1';
+        content.classList.add('flex', 'fade-in');
+      }, 500); 
+    } else {
+      content.style.opacity = '0';
+      setTimeout(() => {
+        content.classList.add('hidden');
+        content.classList.remove('flex', 'fade-in');
+      }, 300); 
+    }
+  }
+}
+
 const openAccordion = (accordion, accordions, title) => {
   accordions.forEach((otherAccordion) => {
-    if (otherAccordion !== accordion && otherAccordion.classList.contains('open')) {
+    if (otherAccordion !== accordion && otherAccordion.classList.contains('h-open')) {
       const otherTitle = otherAccordion.querySelector('.accordion-title');
       closeAccordion(otherAccordion, accordions, otherTitle);
     }
   });
 
-  accordion.classList.add('open');
-  accordion.classList.remove('shrink');
+  accordion.classList.add('h-open');
+  accordion.classList.remove('h-shrink');
   changeBodyColor(accordion);
 
   title.style.opacity = '0';
   setTimeout(() => {
     title.style.display = 'none';
-  }, 300);
+  }, 400);
 
   accordions.forEach((otherAccordion) => {
     if (otherAccordion !== accordion) {
-      otherAccordion.classList.add('shrink');
+      otherAccordion.classList.add('h-shrink');
       const otherTitle = otherAccordion.querySelector('.accordion-title');
       otherTitle.style.display = 'flex';
       setTimeout(() => {
         otherTitle.style.opacity = '1';
-      }, 300);
+      }, 400);
     }
   });
+
+  toggleContent(accordion.classList[1], true);
 }
 
 const closeAccordion = (accordion, accordions, title) => {
-  accordion.classList.remove('open');
-  accordion.classList.remove('shrink');
+  accordion.classList.remove('h-open');
+  accordion.classList.remove('h-shrink');
   resetBodyColor();
 
   setTimeout(() => {
     title.style.display = 'flex';
     setTimeout(() => {
       title.style.opacity = '1';
-    }, 300);
-  }, 300);
+    }, 400);
+  }, 400);
 
   accordions.forEach((otherAccordion) => {
-    otherAccordion.classList.remove('shrink');
+    otherAccordion.classList.remove('h-shrink');
   });
+
+  toggleContent(accordion.classList[1], false);
 }
 
 accordions.forEach((accordion) => {
   const title = accordion.querySelector('.accordion-title');
 
   accordion.addEventListener('click', () => {
-    if (accordion.classList.contains('open')) {
+    if (accordion.classList.contains('h-open')) {
       closeAccordion(accordion, accordions, title);
     } else {
       openAccordion(accordion, accordions, title);
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: {
-				'rubik': ['Rubik Mono One', 'sans-serif']
-			}
-		},
+			extend: {
+					fontFamily: {
+							'rubik': ['Rubik Mono One', 'sans-serif']
+					},
+					height: {
+							'open': '65vh',
+							'shrink': '7vh',
+					},
+					animation: {
+							'fade-in': 'fadeIn 0.5s ease-in-out',
+					},
+					keyframes: {
+							fadeIn: {
+									'0%': { opacity: '0' },
+									'100%': { opacity: '1' },
+							},
+					},
+			},
 	},
 	plugins: [],
 }
