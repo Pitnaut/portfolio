476cb91f4eb344515e5f0d01ee60d3a51b5106f6
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-18 15:15:57 +0200

    first version of contact finnished

--- src/js/modules/contentContact.js
+++ src/js/modules/contentContact.js
@@ -4,8 +4,9 @@ export const showContactContent = (accordion) => {
   const contactContent = accordion.querySelector('.contact-content');
   const contactIntro = accordion.querySelector('.contact-intro');
   const contactIntroSocial = accordion.querySelector('.contact-intro-social');
-  const clickableElements = accordion.querySelectorAll('.contact-tel, .contact-mail, .contact-whatsapp, .contact-telegram, .contact-linkedin');
-  const contactImgs = accordion.querySelectorAll('svg')
+  const contactIntroGithub = accordion.querySelector('.contact-intro-github');
+  const clickableElements = accordion.querySelectorAll('.contact-tel, .contact-mail, .contact-whatsapp, .contact-telegram, .contact-linkedin, .contact-instagram, .contact-bluesky, .contact-github');
+  const contactImgs = accordion.querySelectorAll('svg');
 
   clickableElements.forEach((element) => {
     element.addEventListener('click', function(event) {
@@ -16,6 +17,7 @@ export const showContactContent = (accordion) => {
   animateElement(contactContent, ['flex', 'opacity-100'], ['hidden', 'opacity-0']);
   animateElement(contactIntro, ['opacity-100'], ['opacity-0'], 100);
   animateElement(contactIntroSocial, ['opacity-100'], ['opacity-0'], 100);
+  animateElement(contactIntroGithub, ['opacity-100'], ['opacity-0'], 100);
 
   contactImgs.forEach((svg, index) => {
     animateElement(svg, ['scale-100'], ['scale-0'], 300 + index * 100)
@@ -26,11 +28,13 @@ export const hideContactContent = (accordion) => {
   const contactContent = accordion.querySelector('.contact-content');
   const contactIntro = accordion.querySelector('.contact-intro');
   const contactIntroSocial = accordion.querySelector('.contact-intro-social');
+  const contactIntroGithub = accordion.querySelector('.contact-intro-github');
   const contactImgs = accordion.querySelectorAll('svg')
 
   animateElement(contactContent, ['hidden', 'opacity-0'], ['flex', 'opacity-100'], 400);
   animateElement(contactIntro, ['opacity-0'], ['opacity-100'], 400);
-  animateElement(contactIntroSocial, ['opacity-0'], ['opacity-100'], 400)
+  animateElement(contactIntroSocial, ['opacity-0'], ['opacity-100'], 400);
+  animateElement(contactIntroGithub, ['opacity-0'], ['opacity-100'], 400);
 
   contactImgs.forEach((svg, index) => {
     animateElement(svg, ['scale-0'], ['scale-100'], 300 + index * 100)
