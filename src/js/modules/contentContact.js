51f3359d3caf9067a1a1316f5c43e4aa84051e55
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-18 14:20:25 +0200

    reduce the number of variables into a new one with the clickable elements

--- src/js/modules/contentContact.js
+++ src/js/modules/contentContact.js
@@ -3,43 +3,36 @@ import { animateElement } from "./utils";
 export const showContactContent = (accordion) => {
   const contactContent = accordion.querySelector('.contact-content');
   const contactIntro = accordion.querySelector('.contact-intro');
-  const contactTel = accordion.querySelector('.contact-tel');
-  const contactEmail = accordion.querySelector('.contact-mail');
-  const contactWhatsapp = accordion.querySelector('.contact-whatsapp');
-  const contactTelegram = accordion.querySelector('.contact-telegram');
+  const contactIntroSocial = accordion.querySelector('.contact-intro-social');
+  const clickableElements = accordion.querySelectorAll('.contact-tel, .contact-mail, .contact-whatsapp, .contact-telegram, .contact-linkedin');
+  const contactImgs = accordion.querySelectorAll('svg')
 
-  if (contactTel) {
-    contactTel.addEventListener('click', function(event) {
-        event.stopPropagation();
-    });
-  }
-
-  if (contactEmail) {
-    contactEmail.addEventListener('click', function(event) {
+  clickableElements.forEach((element) => {
+    element.addEventListener('click', function(event) {
       event.stopPropagation();
     });
-  }
-
-  if (contactWhatsapp) {
-    contactWhatsapp.addEventListener('click', function(event) {
-      event.stopPropagation();
-    });
-  }
-
-  if (contactTelegram) {
-    contactTelegram.addEventListener('click', function(event) {
-      event.stopPropagation();
-    });
-  }
+  });
 
   animateElement(contactContent, ['flex', 'opacity-100'], ['hidden', 'opacity-0']);
-  animateElement(contactIntro, ['opacity-100'], ['opacity-0'], 400);
+  animateElement(contactIntro, ['opacity-100'], ['opacity-0'], 100);
+  animateElement(contactIntroSocial, ['opacity-100'], ['opacity-0'], 100);
+
+  contactImgs.forEach((svg, index) => {
+    animateElement(svg, ['scale-100'], ['scale-0'], 300 + index * 100)
+  })
 }
 
 export const hideContactContent = (accordion) => {
   const contactContent = accordion.querySelector('.contact-content');
   const contactIntro = accordion.querySelector('.contact-intro');
+  const contactIntroSocial = accordion.querySelector('.contact-intro-social');
+  const contactImgs = accordion.querySelectorAll('svg')
 
   animateElement(contactContent, ['hidden', 'opacity-0'], ['flex', 'opacity-100'], 400);
   animateElement(contactIntro, ['opacity-0'], ['opacity-100'], 400);
+  animateElement(contactIntroSocial, ['opacity-0'], ['opacity-100'], 400)
+
+  contactImgs.forEach((svg, index) => {
+    animateElement(svg, ['scale-0'], ['scale-100'], 300 + index * 100)
+  })
 }
