465f1b58aa189fc8e1b89923aae235453818bccc
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-19 14:11:47 +0200

    add logic for change images after clicking the project

--- src/js/modules/contentProjects.js
+++ src/js/modules/contentProjects.js
@@ -2,6 +2,22 @@ import { animateElement } from "./utils";
 
 export const showProjectsContent = (accordion) => {
   const projectsContent = accordion.querySelector('.projects-content');
+  const clickableElements = accordion.querySelectorAll('.lotto, .shinny, .todo, .sound, .birds');
+  const images = accordion.querySelectorAll('.img-lotto, .img-shinny, .img-todo, .img-sound, .img-birds');
+
+  clickableElements.forEach((element) => {
+    element.addEventListener('click', function(event) {
+      event.stopPropagation();
+
+      images.forEach((img) => {
+        img.classList.add('hidden');
+      });
+
+      const imgClass = `.img-${element.classList[0]}`;
+      const img = accordion.querySelector(imgClass);
+      img.classList.remove('hidden');
+    });
+  });
 
   animateElement(projectsContent, ['flex', 'opacity-100'], ['hidden', 'opacity-0']);
 }
