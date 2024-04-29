5436c63486972166d5b2a984b471acf6680617dd
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-04-29 16:06:33 +0200

    All sections titles are now responsive and firs versión of the bio responsive

--- src/js/modules/contentBio.js
+++ src/js/modules/contentBio.js
@@ -6,12 +6,12 @@ export const showBioContent = (accordion) => {
   const name = accordion.querySelector('h2');
   const profession = accordion.querySelector('h3');
   const paragraphs = accordion.querySelectorAll('p');
-  
+
   animateElement(bioContent, ['flex', 'opacity-100'], ['hidden', 'opacity-0']);
   animateElement(image, ['scale-100'], ['scale-0'], 300);
   animateElement(name, ['translate-x-0', 'opacity-100'], ['translate-x-full', 'opacity-0'], 500);
   animateElement(profession, ['translate-x-0', 'opacity-100'], ['translate-x-full', 'opacity-0'], 500);
-  
+
   paragraphs.forEach((p, index) => {
     animateElement(p, ['translate-x-0', 'opacity-100'], ['-translate-x-full', 'opacity-0'], 1000 + index * 500);
   });
@@ -23,12 +23,12 @@ export const hideBioContent = (accordion) => {
   const name = accordion.querySelector('h2');
   const profession = accordion.querySelector('h3');
   const paragraphs = accordion.querySelectorAll('p');
-  
+
   animateElement(bioContent, ['hidden', 'opacity-0'], ['flex', 'opacity-100'], 400);
   animateElement(image, ['scale-0'], ['scale-100']);
   animateElement(name, ['translate-x-full', 'opacity-0'], ['translate-x-0', 'opacity-100']);
   animateElement(profession, ['translate-x-full', 'opacity-0'], ['translate-x-0', 'opacity-100']);
-  
+
   paragraphs.forEach((p, index) => {
     animateElement(p, ['-translate-x-full', 'opacity-0'], ['translate-x-0', 'opacity-100'], 500 + index * 500);
   });
--- src/js/modules/contentProjects.js
+++ src/js/modules/contentProjects.js
@@ -6,16 +6,16 @@ export const showProjectsContent = (accordion) => {
   const images = accordion.querySelectorAll('.img-lotto, .img-shinny, .img-todo, .img-sound, .img-birds');
 
   clickableElements.forEach((element, index) => {
-    // Añade la animación aquí
+
     animateElement(element, ['opacity-100'], ['opacity-0'], 200 + index * 200);
-  
-    element.addEventListener('click', function(event) {
+
+    element.addEventListener('click', function (event) {
       event.stopPropagation();
-  
+
       images.forEach((img) => {
         img.classList.add('hidden');
       });
-  
+
       const imgClass = `.img-${element.classList[0]}`;
       const img = accordion.querySelector(imgClass);
       img.classList.remove('hidden');
@@ -23,23 +23,27 @@ export const showProjectsContent = (accordion) => {
   });
 
   images.forEach((img) => {
-    img.addEventListener('click', function(event) {
+    img.addEventListener('click', function (event) {
       event.stopPropagation();
     });
+    animateElement(img, ['scale-100'], ['scale-0'], 1200)
   });
-
   animateElement(projectsContent, ['flex', 'opacity-100'], ['hidden', 'opacity-0']);
 }
-
 
 export const hideProjectsContent = (accordion) => {
   const projectsContent = accordion.querySelector('.projects-content');
   const clickableElements = accordion.querySelectorAll('.lotto, .shinny, .todo, .sound, .birds');
+  const images = accordion.querySelectorAll('.img-lotto, .img-shinny, .img-todo, .img-sound, .img-birds');
 
   animateElement(projectsContent, ['hidden', 'opacity-0'], ['flex', 'opacity-100'], 400);
 
   clickableElements.forEach((element) => {
     animateElement(element, ['opacity-0'], ['opacity-100'], 0);
+  });
+
+  images.forEach((img) => {
+    animateElement(img, ['scale-0'], ['scale-100'], 0);
   });
 
 }
